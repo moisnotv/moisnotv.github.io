edfb0142e4240650361e28d2da8f7e5469df037d
moisnotv <57194419+moisnotv@users.noreply.github.com>
2019-10-30 16:07:48 -0300

    Add files via upload

---
+++ tigo.php
@@ -0,0 +1,41 @@
+<head>
+    <title>test</title>
+<meta charset="utf-8">
+<meta content="max-age=200" http-equiv="Cache-Control">
+<meta content="noindex,nofollow" name="robots">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<link rel="stylesheet" href="https://cdn.fluidplayer.com/v2/current/fluidplayer.min.css" type="text/css"/>
+<script src="https://cdn.fluidplayer.com/v2/current/fluidplayer.min.js"></script>
+
+<center><video id='hls-video'>
+    <source src='http://epro.webhop.biz:25461/live/colom92hdshaslk/gMrpA2ab/7482.m3u8' type='application/x-mpegURL'/>
+</video></center>
+
+<script>
+fluidPlayer(
+    'hls-video',
+    {
+        layoutControls: {
+            fillToContainer: true,
+			autoPlay: true,
+			playButtonShowing: true,
+		playPauseAnimation: true,
+		mute: false,
+		logo: {
+			imageUrl: 'http://mensajerofm.org/app/IPTV/drawer_header.png',
+			position: 'bottom right',
+			clickUrl: 'https://www.moisnotv.ml/',
+			opacity: 1,
+			mouseOverImageUrl: 'http://mensajerofm.org/app/IPTV/drawer_header.png',
+			imageMargin: '2px',
+			hideWithControls: false,
+			showOverAds: false
+		},
+	  }
+    }
+);
+</script>
+
+
+</html>
